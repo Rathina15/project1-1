bb851d47a1c6ed216c6e578d7c14e052e163b01e
keerthana <keerthanarghu14@gmail.com>
2022-01-03 14:26:55 +0530

    first commit

---
+++ myproject/src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header class="header">
+        </header>
+      
+      <br></br>
+        
+        <form>
+        <div className="App1">   
+        Login Form
+        </div>
+        <br></br>
+
+        <br></br>
+       <center>
+          <input type="text" placeholder="email or username"></input><br></br>
+            
+            <input id="pwd" type="password" placeholder="password"></input><br></br>
+            
+            <input type="submit" value="sign in"></input> 
+            <br></br>
+            <br></br>
+            <a href="#">forget password?</a></center>
+
+
+
+        </form>
+
+        <br></br>
+
+        
+    
+    </div>
+  );
+}
+
+export default App;
