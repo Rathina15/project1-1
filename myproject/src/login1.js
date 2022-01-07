d5891164c9d9f39dc5da70244e279e11bd9f3427
keerthana <keerthanarghu14@gmail.com>
2022-01-07 10:39:10 +0530

    commit 5

--- myproject/src/login1.js
+++ myproject/src/login1.js
@@ -1,12 +1,12 @@
 export function Login1(){
     return(
   <div className="user1">
-      <div>
+      
        <h1>username</h1>
        
        <input type="text" placeholder="email or username"></input><br></br>
             
-       </div>
+       
        </div>      
     );
 }
--- myproject/src/login2.js
+++ myproject/src/login2.js
@@ -1,12 +1,12 @@
 export function Login2(){
     return(
   <div className="pwd1">
-      <div>
+      
        <h1>password</h1>
        
        <input id="pwd" type="password" placeholder="password"></input><br></br>
  
-       </div>
+       
        </div>      
     );
 }
